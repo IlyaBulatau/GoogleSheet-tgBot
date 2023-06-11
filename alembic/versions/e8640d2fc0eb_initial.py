6abad4e982df45903732c953234ca715acc64336
IlyaBulatau <ilyabulatau@gmail.com>
2023-06-11 21:49:42 +0200

    add alembic

---
+++ alembic/versions/e8640d2fc0eb_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: e8640d2fc0eb
+Revises: 
+Create Date: 2023-06-11 21:46:07.756098
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e8640d2fc0eb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,6 @@
 from database.connect import Base, session
 import sqlalchemy as db
+from sqlalchemy.orm import relationship 
 
 from logger.logger import logger
 
@@ -9,7 +10,6 @@ class BaseModel:
         try:
             session.add(self)
             session.commit()
-            logger.warning(f'ADD NEW USER WITH USERNAME: {self.username}, ID: {self.tg_id}')
         except:
             session.rollback()
 
@@ -20,6 +20,7 @@ class User(Base, BaseModel):
     tg_id = db.Column(db.BigInteger(), unique=True, nullable=False)
     username = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), nullable=True)
+    tablse = relationship('Table', backref='user')
 
     def save_email(self, **kwargs):
         for k, v in kwargs.items():
@@ -27,8 +28,19 @@ class User(Base, BaseModel):
             logger.info(f'User with {self.tg_id} add email: {v}')
         session.commit()
 
+    def save(self):
+        super(User, self).save(self)
+        logger.warning(f'ADD NEW USER WITH USERNAME: {self.username}, ID: {self.tg_id}')
+
     @classmethod
     def get_user_by_id(cls, tg_id):
         user = cls.query.filter(cls.tg_id == tg_id).first()
         return user
+    
+class Table(Base, BaseModel):
+    __tablename__ = 'tablse'
+
+    id = db.Column(db.BigInteger(), primary_key=True)
+    url = db.Column(db.String(), nullable=False)
+    user_tg_id = db.Column(db.BigInteger(), db.ForeignKey('users.tg_id'))
     
